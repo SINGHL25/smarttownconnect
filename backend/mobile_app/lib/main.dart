a8d2a2d45c3aab1ba830fbb4e4bb5081605dfec0
Akhilesh <akhi.singh1989@gmail.com>
2025-07-08 21:43:19 +1000

    Update main.dart

--- backend/mobile_app/lib/main.dart
+++ backend/mobile_app/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(SmartTownConnectApp());
+}
+
+class SmartTownConnectApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SmartTownConnect',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
 
