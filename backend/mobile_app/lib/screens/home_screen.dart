d7797d7834800071ea8a4d3b5d5f7c3e9163b08c
Akhilesh <akhi.singh1989@gmail.com>
2025-07-08 21:43:45 +1000

    Update home_screen.dart

--- backend/mobile_app/lib/screens/home_screen.dart
+++ backend/mobile_app/lib/screens/home_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("SmartTownConnect")),
+      drawer: CustomDrawer(),
+      body: Center(
+        child: Text("Welcome to SmartTownConnect!",
+            style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
