1aab468a971471f6d6869c33d9e0de442d669dbc
Akhilesh <akhi.singh1989@gmail.com>
2025-07-08 21:45:03 +1000

    Update discussion_screen.dart

--- backend/mobile_app/lib/screens/discussion_screen.dart
+++ backend/mobile_app/lib/screens/discussion_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DiscussionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Resident Discussions")),
+      body: Center(
+        child: Text("Coming Soon: Community forums and discussions."),
+      ),
+    );
+  }
+}
 
